4b71e73dfb491fbcc18d5f91d4df35debcb13cf7
lishanjun <1046928300@qq.com>
2020-11-09 21:45:47 +0800

    备用交互

--- components/indexItem/indexItem.js
+++ components/indexItem/indexItem.js
@@ -13,18 +13,21 @@ Component({
     }
   },
   data: {
+    like: null,
+    likeNum: 0,
+    hateNum: 0,
     result: {},
     index: 0
   },
   attached () {
     const {item, idx} = this.properties
-    this.setData({result: item, index: idx})
+    this.setData({result: item, index: idx, likeNum: item.likeNum, like: item.like, hateNum: item.hateNum})
   },
   methods: {
     likes (e) {
       const { id, status } = e.currentTarget.dataset
       console.log('status:', typeof status)
-      const result = this.data.result
+      const { result, likeNum } = this.data
       if (status === true) {
         this.likesCancel(id, false, 'like')
       } else {
@@ -34,20 +37,32 @@ Component({
             secretId: id,
             isLike: status ? !status : true
           }
-        }).then(() => {
-          this.setData({result: {
-            ...result,
-            like: true,
-            likeNum: result.likeNum + 1
-          }})
+        }).then((res) => {
+          const { code, content } = res
+          if (code === 0) {
+            this.setData({
+              result: {
+                ...result,
+                like: true,
+                likeNum: result.likeNum + 1
+              },
+              likeNum: likeNum + 1,
+              like: true
+            })
+          } else {
+            wx.showToast({
+              title: content,
+              icon: 'none'
+            })
+          }
           console.log('result:', this.data.result)
         })
       }
     },
-    async noLikes (e) {
+    noLikes (e) {
       const { id, status } = e.currentTarget.dataset
       console.log('status:', typeof status, status)
-      const result = this.data.result
+      const { result, hateNum } = this.data
       if (status === false) {
         this.likesCancel(id, true)
       } else {
@@ -57,12 +72,24 @@ Component({
             secretId: id,
             isLike: status ? !status : false
           }
-        }).then(() => {
-          this.setData({result: {
-            ...result,
-            like: false,
-            hateNum: result.hateNum + 1
-          }})
+        }).then((res) => {
+          const { code, content } = res
+          console.log('dddss:', res)
+          if (code === 0) {
+            this.setData({
+              result: {
+                ...result,
+                like: false,
+                hateNum: result.hateNum + 1
+              },
+              hateNum: hateNum + 1,
+              like: false})
+          } else {
+            wx.showToast({
+              title: content,
+              icon: 'none'
+            })
+          }
         })
       }
     },
@@ -73,20 +100,34 @@ Component({
           secretId: id,
           isLike: status
         }
-      }).then(() => {
-        const result = this.data.result
-        if (isLike) {
-          this.setData({result: {
-            ...result,
-            like: null,
-            likeNum: result.likeNum - 1
-          }})
+      }).then((res) => {
+        const { code, content } = res
+        if (code === 0) {
+          const { result, hateNum, likeNum } = this.data
+          if (isLike) {
+            this.setData({
+              result: {
+                ...result,
+                like: null,
+                likeNum: result.likeNum - 1
+              },
+              like: null,
+              likeNum: likeNum - 1})
+          } else {
+            this.setData({
+              result: {
+                ...result,
+                like: null,
+                hateNum: result.hateNum - 1
+              },
+              like: null,
+              hateNum: hateNum - 1})
+          }
         } else {
-          this.setData({result: {
-            ...result,
-            like: null,
-            hateNum: result.hateNum - 1
-          }})
+          wx.showToast({
+            title: content,
+            icon: 'none'
+          })
         }
       })
     },
